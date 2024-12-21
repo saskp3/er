0fd214717d178807b183f8a16f2aabbed14c3b6a
saskp3 <105058505+saskp3@users.noreply.github.com>
2024-12-21 14:50:12 +0300

    Add files via upload

--- cheat.lua
+++ cheat.lua
@@ -1,4 +1,28 @@
-﻿loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/BTools.txt"))()
+﻿--[[
+A distribution of https://wearedevs.net/scripts
+Last updated August 2, 2021
+
+Description: Gives your player the old system of building tools
+
+Instruction: Inject this script into any game using a Lua executor like JJSploit. 
+]]
+
+backpack = game:GetService("Players").LocalPlayer.Backpack
+
+hammer = Instance.new("HopperBin")
+hammer.Name = "Hammer"
+hammer.BinType = 4
+hammer.Parent = backpack
+
+cloneTool = Instance.new("HopperBin")
+cloneTool.Name = "Clone"
+cloneTool.BinType = 3
+cloneTool.Parent = backpack
+
+grabTool = Instance.new("HopperBin")
+grabTool.Name = "Grab"
+grabTool.BinType = 2
+grabTool.Parent = backpack
 --[[
 WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
 ]]
