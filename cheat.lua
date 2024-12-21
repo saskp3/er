44d037123f4c06f54451bd2216ec58704bdc8e82
saskp3 <105058505+saskp3@users.noreply.github.com>
2024-12-21 14:43:52 +0300

    Add files via upload

---
+++ cheat.lua
@@ -0,0 +1,10 @@
+﻿loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/BTools.txt"))()
+--[[
+WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+if InfiniteJumpEnabled then
+game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+end
+end)
